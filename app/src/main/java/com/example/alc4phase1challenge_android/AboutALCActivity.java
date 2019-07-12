cc027becf52eed5a55097c913e34451a2a869d22
Oluwaseun liadi <liadioluwaseun@gmail.com>
2019-07-12 23:44:32 +0100

    basic implementation completed

--- app/src/main/java/com/example/alc4phase1challenge_android/AboutALCActivity.java
+++ app/src/main/java/com/example/alc4phase1challenge_android/AboutALCActivity.java
@@ -2,14 +2,31 @@ package com.example.alc4phase1challenge_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.http.SslError;
 import android.os.Bundle;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class AboutALCActivity extends AppCompatActivity {
+
+    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_alc);
         getSupportActionBar().setTitle(R.string.about_alc_toolbar_text);
+        mWebView = findViewById(R.id.about_alc_webview);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                //super.onReceivedSslError(view, handler, error);
+                handler.proceed();
+            }
+        });
+        mWebView.loadUrl("https://andela.com/alc/");
     }
 }
--- app/src/main/java/com/example/alc4phase1challenge_android/ProfileActivity.java
+++ app/src/main/java/com/example/alc4phase1challenge_android/ProfileActivity.java
@@ -3,13 +3,18 @@ package com.example.alc4phase1challenge_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    private ImageView mProfileImageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         getSupportActionBar().setTitle(R.string.profile_toolbar_text);
+        mProfileImageView = findViewById(R.id.profile_image);
     }
 }
