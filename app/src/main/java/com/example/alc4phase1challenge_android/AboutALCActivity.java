9f23422a4d191c2189b9fff3a6cc95adae83a9c7
Oluwaseun liadi <liadioluwaseun@gmail.com>
2019-07-14 12:36:33 +0100

    implemented land for about alc

--- app/src/main/java/com/example/alc4phase1challenge_android/AboutALCActivity.java
+++ app/src/main/java/com/example/alc4phase1challenge_android/AboutALCActivity.java
@@ -1,10 +1,13 @@
 package com.example.alc4phase1challenge_android;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.net.http.SslError;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.webkit.SslErrorHandler;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -18,8 +21,21 @@ public class AboutALCActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_alc);
         getSupportActionBar().setTitle(R.string.about_alc_toolbar_text);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         mWebView = findViewById(R.id.about_alc_webview);
         mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+            }
+
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                super.onReceivedTitle(view, title);
+                getSupportActionBar().setSubtitle(title);
+            }
+        });
         mWebView.setWebViewClient(new WebViewClient(){
             @Override
             public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
@@ -27,6 +43,15 @@ public class AboutALCActivity extends AppCompatActivity {
                 handler.proceed();
             }
         });
+
         mWebView.loadUrl("https://andela.com/alc/");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home ) {
+            finish();
+        }
+        return true;
     }
 }
--- app/src/main/java/com/example/alc4phase1challenge_android/ProfileActivity.java
+++ app/src/main/java/com/example/alc4phase1challenge_android/ProfileActivity.java
@@ -1,8 +1,10 @@
 package com.example.alc4phase1challenge_android;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -15,6 +17,15 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         getSupportActionBar().setTitle(R.string.profile_toolbar_text);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         mProfileImageView = findViewById(R.id.profile_image);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home ) {
+            finish();
+        }
+        return true;
     }
 }
